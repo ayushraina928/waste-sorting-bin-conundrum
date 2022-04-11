4f38b44827201ccd9d04d10df89f17e67d668d09
Ayush Raina <raina.ayush77@gmail.com>
2022-04-11 07:22:46 -0700

    Adding Contents for Docker Container

---
+++ webapp/object_detection/app.py
@@ -0,0 +1,9 @@
+import skimage.transform
+import tensorflow as tf
+import keras 
+import keras.backend as K
+import keras.layers as KL
+import keras.engine as KE
+import keras.models as KM
+while(True):
+    print ("hello there")
