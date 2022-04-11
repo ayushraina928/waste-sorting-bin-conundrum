0ac4ee257912109037be5ab57083bdf00f889cc9
tja34 <tja34@sfu.ca>
2022-04-11 07:32:46 -0700

    Adding files

---
+++ webapp/detect.sh
@@ -0,0 +1,2 @@
+docker container run --rm -v ${PWD}/images:/code/detector/test_images -e uuid=${1} --name detect vader404/733detect:new
+#sha256:a14a2ce5da4a9a9a252018c45cb4dbfe488d858397cffa8fb0549f1d3d3accc6
